707bc179e1ab10023689db8f8bedac6d59839461
mateusLucSantos <mateuslucsantos.dev@gmail.com>
2025-03-12 22:50:51 -0300

    teste

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,8 +1,11 @@
+import { Button } from "../../components/ui/button";
+
 export function SignIn() {
   return (
     <div>
-      <div>
+      <div className="flex flex-col items-center justify-center h-screen">
         <h1>Sign In</h1>
+        <Button>Entrar</Button>
       </div>
     </div>
   );
