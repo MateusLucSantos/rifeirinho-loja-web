b2ec3dbc208c2e61ee7a6347beeb998eb28c28aa
MateusLucSantos <mateuslucsantos.dev@gmail.com>
2025-03-21 09:21:04 -0300

    Dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes/routes";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 function App() {
   return (
     <>
-      <Toaster richColors position="top-right" />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="rifeirinho-theme" defaultTheme="light">
+        <Toaster richColors position="top-right" />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </>
   );
 }
--- src/pages/_layout/app.tsx
+++ src/pages/_layout/app.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar";
+import { ThemeToggle } from "@/components/theme/theme-toggle";
 import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
@@ -12,10 +13,13 @@ export function AppLayout() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="bg-muted flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <header className="bg-muted flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
+          </div>
+          <div className="mr-6">
+            <ThemeToggle />
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
