aee14530a465bf58e757436f7eb80fd886789d7a
mateusLucSantos <mateuslucsantos.dev@gmail.com>
2025-03-25 22:26:24 -0300

    Alteração na altura do Header da página

--- src/pages/_layout/app.tsx
+++ src/pages/_layout/app.tsx
@@ -13,7 +13,7 @@ export function AppLayout() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="bg-muted dark:bg-input flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <header className="bg-muted dark:bg-input flex h-12 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
