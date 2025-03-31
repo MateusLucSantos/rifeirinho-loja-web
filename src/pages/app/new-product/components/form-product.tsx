f00bbb26e7684d68904bff34c2ab81e53e8a78d0
MateusLucSantos <mateuslucsantos.dev@gmail.com>
2025-03-31 11:22:20 -0300

    Tela de cadastro de novo produto.

--- src/pages/app/new-product/components/form-product.tsx
+++ src/pages/app/new-product/components/form-product.tsx
@@ -1,7 +1,10 @@
 import { DatePicker } from "@/components/date-picker";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Barcode } from "lucide-react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -9,17 +12,20 @@ import { z } from "zod";
 const schema = z.object({
   id: z.string().nonempty(),
   name: z.string().nonempty(),
-  cpf: z.string().regex(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/),
-  nickname: z.string().nonempty(),
-  email: z.string().email(),
-  phone: z.string().regex(/^\d{10}$/),
-  address: z.string().nonempty(),
-  number_address: z.string().nonempty(),
-  complement: z.string().optional(),
-  neighborhood: z.string().optional(),
-  city: z.string().nonempty(),
-  uf: z.string().nonempty(),
-  zip_code: z.string().regex(/^\d{5}-\d{3}$/),
+  code: z.string().nonempty(),
+  unit: z.string().nonempty(),
+  price_sell: z.string().nonempty(),
+  description: z.string().nonempty(),
+  stock: z.string().nonempty(),
+  stock_min: z.string().nonempty(),
+  stock_max: z.string().nonempty(),
+  barcode: z.string().nonempty(),
+  brand: z.string().nonempty(),
+  category: z.string().nonempty(),
+  supplier: z.string().nonempty(),
+  image: z.string().nonempty(),
+  createdAt: z.string().nonempty(),
+  updatedAt: z.string().nonempty(),
 });
 type ClientForm = z.infer<typeof schema>;
 
@@ -31,142 +37,151 @@ export function FormProduct() {
   const [idValue, setIdValue] = useState("");
   return (
     <>
-      <form action="">
-        <div className="m-auto space-y-4 p-4 xl:w-[1024px]">
-          <div className="flex items-center justify-between">
-            <div className="flex flex-col space-y-2">
-              <Label htmlFor="id">ID</Label>
-              <Input
-                className="min-w-[40px]"
-                type="text"
-                id="id"
-                {...register("id")}
-                value={idValue}
-                style={{
-                  width: `${idValue.length * 15}px`,
-                }}
-                onChange={(e) => setIdValue(e.target.value)}
-              />
+      <form action="" className="h-screen w-full">
+        <ScrollArea
+          type="always"
+          className="h-[500px] w-full overflow-y-auto 2xl:h-screen" // Altura total da tela e rolagem vertical
+        >
+          <div className="m-auto space-y-4 p-4 xl:w-[1024px]">
+            <div className="flex items-center justify-between">
+              <div className="flex flex-col space-y-2">
+                <Label htmlFor="id">ID</Label>
+                <Input
+                  className="min-w-[40px]"
+                  type="text"
+                  id="id"
+                  {...register("id")}
+                  value={idValue}
+                  style={{
+                    width: `${idValue.length * 15}px`,
+                  }}
+                  onChange={(e) => setIdValue(e.target.value)}
+                />
+              </div>
+              <div className="flex w-[150px] flex-col space-y-2 lg:w-[500px]">
+                <Label htmlFor="code">Código</Label>
+                <Input
+                  className="min-w-[120px]"
+                  type="text"
+                  id="code"
+                  {...register("code")}
+                />
+              </div>
+              <div className="flex flex-col space-y-2">
+                <Label htmlFor="createdAt">Data de criação</Label>
+                <DatePicker />
+              </div>
             </div>
-            <div className="flex w-[300px] flex-col space-y-2 lg:w-[500px]">
-              <Label htmlFor="nickname">Apelido</Label>
-              <Input
-                className="min-w-[120px]"
-                type="text"
-                id="nickname"
-                {...register("nickname")}
-              />
-            </div>
-            <div className="flex flex-col space-y-2">
-              <Label htmlFor="name">Data de nascimento</Label>
-              <DatePicker />
-            </div>
-          </div>
 
-          <div className="flex justify-between space-x-10">
-            <div className="flex w-[400px] flex-col space-y-2 lg:w-[800px]">
+            <div className="flex flex-col space-y-2 lg:w-full">
               <Label htmlFor="name">Nome</Label>
               <Input type="text" id="name" {...register("name")} />
             </div>
-            <div className="flex w-[220px] flex-col space-y-2">
-              <Label htmlFor="cpf">CPF</Label>
-              <Input
-                className="min-w-[120px]"
-                type="text"
-                id="cpf"
-                {...register("cpf")}
-              />
-            </div>
-          </div>
 
-          <div className="flex justify-between space-x-10">
-            <div className="flex w-[400px] flex-col space-y-2 lg:w-[800px]">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                className="min-w-[120px]"
-                type="text"
-                id="email"
-                {...register("email")}
-              />
+            <div className="flex justify-between space-x-10">
+              <div className="flex flex-col space-y-2 lg:w-[300px]">
+                <Label htmlFor="unit">Unidade</Label>
+                <Input
+                  className="min-w-[120px]"
+                  type="text"
+                  id="unit"
+                  {...register("unit")}
+                />
+              </div>
+              <div className="flex flex-col space-y-2 lg:w-[500px]">
+                <Label htmlFor="category">Categoria</Label>
+                <Input
+                  className="min-w-[120px]"
+                  type="text"
+                  id="category"
+                  {...register("category")}
+                />
+              </div>
+
+              <div className="flex flex-col space-y-2 lg:w-[500px]">
+                <Label htmlFor="brand">Marca</Label>
+                <Input
+                  className="min-w-[120px]"
+                  type="text"
+                  id="brand"
+                  {...register("brand")}
+                />
+              </div>
+              <div className="flex flex-col space-y-2 lg:w-[500px]">
+                <Label htmlFor="supplier">Fornecedor</Label>
+                <Input
+                  className="min-w-[80px]"
+                  type="text"
+                  id="supplier"
+                  {...register("supplier")}
+                />
+              </div>
+            </div>
+            <div className="flex justify-between space-x-6">
+              <div className="flex flex-col space-y-2">
+                <Label htmlFor="price_sell">Preço de compra</Label>
+                <Input
+                  className="min-w-[120px]"
+                  type="text"
+                  id="price_sell"
+                  {...register("price_sell")}
+                />
+              </div>
+              <div className="flex flex-col space-y-2">
+                <Label htmlFor="stock">Estoque</Label>
+                <Input
+                  className="min-w-[120px]"
+                  type="text"
+                  id="stock"
+                  {...register("stock")}
+                />
+              </div>
+              <div className="flex flex-col space-y-2">
+                <Label htmlFor="stock_min">Estoque mínimo</Label>
+                <Input
+                  className="min-w-[120px]"
+                  type="text"
+                  id="stock_min"
+                  {...register("stock_min")}
+                />
+              </div>
+              <div className="flex flex-col space-y-2">
+                <Label htmlFor="stock_max">Estoque máximo</Label>
+                <Input
+                  className="min-w-[120px]"
+                  type="text"
+                  id="stock_max"
+                  {...register("stock_max")}
+                />
+              </div>
+            </div>
+            <div className="flex items-center gap-3 space-y-2">
+              <div className="flex w-[464px] flex-col space-y-2 lg:w-full">
+                <Label htmlFor="barcode">Código de Barras</Label>
+                <Input
+                  className="min-w-[120px]"
+                  type="text"
+                  id="barcode"
+                  {...register("barcode")}
+                />
+              </div>
+              <Button className="mt-3">
+                <Barcode />
+                Gerar código de barras
+              </Button>
             </div>
             <div className="flex flex-col space-y-2">
-              <Label htmlFor="phone">Telefone</Label>
+              <Label htmlFor="description">Descrição</Label>
               <Input
-                className="min-w-[220px]"
+                className="h-40 min-w-[120px]"
                 type="text"
-                id="phone"
-                {...register("phone")}
+                id="description"
+                {...register("description")}
               />
             </div>
+            <ScrollBar orientation="vertical" />
           </div>
-          <div className="flex justify-between space-x-10">
-            <div className="flex flex-col space-y-2 lg:w-[600px]">
-              <Label htmlFor="address">Endereço</Label>
-              <Input
-                className="min-w-[120px]"
-                type="text"
-                id="address"
-                {...register("address")}
-              />
-            </div>
-            <div className="flex flex-col space-y-2 lg:w-[80px]">
-              <Label htmlFor="number_address">Número</Label>
-              <Input
-                className="min-w-[80px]"
-                type="text"
-                id="number_address"
-                {...register("number_address")}
-              />
-            </div>
-            <div className="flex flex-col space-y-2 lg:w-[220px]">
-              <Label htmlFor="complement">Complemento</Label>
-              <Input
-                className="min-w-[120px]"
-                type="text"
-                id="complement"
-                {...register("complement")}
-              />
-            </div>
-          </div>
-          <div className="flex justify-between space-x-6">
-            <div className="flex flex-col space-y-2 lg:w-[400px]">
-              <Label htmlFor="neighborhood">Bairro</Label>
-              <Input
-                className="min-w-[120px]"
-                type="text"
-                id="neighborhood"
-                {...register("neighborhood")}
-              />
-            </div>
-            <div className="flex flex-col space-y-2 lg:w-[300px]">
-              <Label htmlFor="city">Cidade</Label>
-              <Input
-                className="min-w-[120px]"
-                type="text"
-                id="city"
-                {...register("city")}
-              />
-            </div>
-            <div className="flex w-[80px] flex-col space-y-2">
-              <Label htmlFor="uf">UF</Label>
-              <Input
-                className="min-w-[80px]"
-                type="text"
-                id="uf"
-                {...register("uf")}
-              />
-            </div>
-            <div className="flex w-[120px] flex-col space-y-2">
-              <Label htmlFor="zip_code">CEP</Label>
-              <Input
-                className="min-w-[120px]"
-                type="text"
-                id="zip_code"
-                {...register("zip_code")}
-              />
-            </div>
-          </div>
-        </div>
+        </ScrollArea>
       </form>
     </>
   );
--- src/pages/app/new-product/new-client.tsx
+++ src/pages/app/new-product/new-client.tsx
@@ -1,11 +1,13 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { FormProduct } from "./components/form-product";
 import { HeaderProduct } from "./components/header-product";
 
 export function NewProduct() {
   return (
-    <>
+    <div>
       <HeaderProduct />
+
       <FormProduct />
-    </>
+    </div>
   );
 }
